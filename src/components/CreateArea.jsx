085730ea8c7eef40ac8debbf14c4145e93580bad
Jabulani Nyawera <jaybcoding@yahoo.com>
2021-05-18 23:06:57 -0500

    Upload note-keeper

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  const [noteExpanded, setNoteExpand] = useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  function changeNoteSize() {
+    setNoteExpand((prevVal) => !prevVal);
+  }
+  console.log(noteExpanded);
+
+  return (
+    <div>
+      <form className="create-note">
+        {noteExpanded ? (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        ) : null}
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          onClick={changeNoteSize}
+          rows="3"
+        />
+        <Zoom in={true}>
+          <Fab onClick={submitNote}>
+            <AddCircleOutlineIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
